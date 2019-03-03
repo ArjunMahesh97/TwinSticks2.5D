6fe239f84db7f8af0b1e6be7d7444a847a22dd26
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-03 19:11:09 +0530

    Replay System

---
+++ Assets/Player/ReplaySystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Record ();
+	}
+
+	void Playback(){
+		rigidBody.isKinematic = true;
+		int frame =	Time.frameCount % bufferFrames;
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+		
+
+	void Record ()
+	{
+		rigidBody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+}
+
+public class MyKeyFrame{
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float aTime, Vector3 aPos, Quaternion aRot){
+		frameTime = aTime;
+		position = aPos;
+		rotation = aRot;
+	}
+}
