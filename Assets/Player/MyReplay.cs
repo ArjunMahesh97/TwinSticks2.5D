b467752b851e65276638d9ab0dd53385106aada8
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-03 10:02:07 +0530

    KeyFrame class created

---
+++ Assets/Player/MyReplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MyKeyFrame testKey = new MyKeyFrame (1.0f, Vector3.up, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class MyKeyFrame{
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float aTime, Vector3 aPos, Quaternion aRot){
+		frameTime = aTime;
+		position = aPos;
+		rotation = aRot;
+	}
+}
